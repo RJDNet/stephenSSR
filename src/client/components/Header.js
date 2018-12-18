fbe6a6abbbc56c343f89415efa58dc90ed118f50
RJDNet <russdando86@gmail.com>
2018-12-18 15:57:50 +0000

    Initial Commit

---
+++ src/client/components/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
+const Header = ({ auth }) => {
+  const authButton = auth ? (
+    <a href="/api/logout">Logout</a>
+  ) : (
+      <a href="/api/auth/google">Login</a>
+    );
+
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <Link to="/" className="brand-logo">
+          React SSR
+        </Link>
+        <ul className="right">
+          <li>
+            <Link to="/users">Users</Link>
+            {/* <Redirect to='/users'>Users</Redirect> */}
+            {/* <a href="/users">Users</a> */}
+          </li>
+          <li>
+            <Link to="/admins">Admins</Link>
+          </li>
+          <li>{authButton}</li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
